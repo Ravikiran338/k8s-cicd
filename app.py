d52835b0cded210b79bdc6801d615a177c98805e
Ravikiran338 <moolaravikiran@gmail.com>
2019-08-20 23:30:27 +0530

    creating python script to check the login status

---
+++ app.py
@@ -0,0 +1,39 @@
+import requests, json
+import logging
+import sys
+from pprint import pprint
+#setup logging
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s %(levelname)s %(message)s',
+                filename='Restapi_validation.log',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filemode='w')
+service_status = ""
+url =  "http://a101d6968c33c11e9802b0e65d0987dc-46880353.us-east-1.elb.amazonaws.com:8090/index.html"
+headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
+logging.info('service url: %s' %(url))
+try:
+        r = requests.get(url, headers=headers, verify=False)
+        logging.info("service response %s" % (r.status_code))
+        if str(r.status_code) != '200' or r.status_code == '':
+                pprint('failed response %s' %r.status_code)
+                service_status = "failed"
+        elif str(r.status_code) == '200':
+                pprint('success response %s' %r.status_code)
+                service_status = "success"
+        else:
+                service_status = "failed"
+                logging.error("error" )
+                sys.exit(1)
+except Exception as e:
+
+    logging.error(e)
+    pprint(e)
+    service_status = "failed"
+    print(service_status)
+    sys.exit(1)
+if service_status == "success":
+        pprint("retuning success status")
+        sys.exit(0)
+else:
+        sys.exit(1)
